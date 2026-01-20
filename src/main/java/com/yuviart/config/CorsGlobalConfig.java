d6a85c9fb9614ed216c25905b767351521a61bc2
Nikhil Raj Mali <nikhilrajmali042@gmail.com>
2026-01-20 16:48:12 +0530

    Added login API and fixed DB connection issue

--- src/main/java/com/yuviart/config/CorsGlobalConfig.java
+++ src/main/java/com/yuviart/config/CorsGlobalConfig.java
@@ -14,7 +14,7 @@ public class CorsGlobalConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
+        .allowedOriginPatterns(  // ✅ Changed from allowedOrigins to allowedOriginPatterns
                     "http://localhost:5173",
                     "http://localhost:5174",
                     "http://localhost:5175",
