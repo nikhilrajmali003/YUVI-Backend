ebbbd4d25fbd62272648e5be45b003179353acd7
Nikhil Raj Mali <nikhilrajmali042@gmail.com>
2026-01-20 10:09:12 +0530

    Final updatation for review section

--- src/main/java/com/yuviart/controller/TestimonialController.java
+++ src/main/java/com/yuviart/controller/TestimonialController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/testimonials")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174", "http://localhost:5175"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://yuviart.netlify.app","https://*.netlify.app"}, allowCredentials = "true")
 public class TestimonialController {
 
     private final TestimonialRepository testimonialRepository;
